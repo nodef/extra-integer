cfa2e487a684ed62ed42177ab4c6ce66c74c8193
Subhajit Sahu <wolfram77@gmail.com>
2020-06-07 02:47:51 +0530

    :watermelon: cool fruit code rebuild

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
+export {default as MAX_VALUE} from './MAX_VALUE';
+export {default as MIN_VALUE} from './MIN_VALUE';
 export {default as abs} from './abs';
 export {default as is} from './is';
 export {default as isPow2} from './isPow2';
 export {default as log10} from './log10';
 export {default as log2} from './log2';
-export {default as MAX_VALUE} from './MAX_VALUE';
-export {default as MIN_VALUE} from './MIN_VALUE';
 export {default as nextPow2} from './nextPow2';
 export {default as pow10} from './pow10';
 export {default as pow2} from './pow2';
